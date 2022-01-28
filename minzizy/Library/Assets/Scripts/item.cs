be500246f886a1ccb2059254b6e8ed2754dd5a44
MINZIZY0426 <98074862+MINZIZY0426@users.noreply.github.com>
2022-01-28 10:10:21 +0900

    캐릭터, 배경 등 적용

---
+++ minzizy/Library/Assets/Scripts/item.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour
+{
+    public string type;
+    Rigidbody2D rigid;
+    public GameObject slotItem;
+
+    // Update is called once per frame
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = Vector2.down * 2;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")   //빗방울 플레이어와 충돌 시
+        {
+
+            Inventory inven = other.GetComponent<Inventory>();
+            for (int i = 0; i < inven.slots.Count; i++)
+            {
+                if (inven.slots[i].isEmpty)
+                {
+                    Instantiate(slotItem, inven.slots[i].slotObj.transform, false);
+                    inven.slots[i].isEmpty = false;
+
+                    break;
+                }
+            }
+
+
+            ScoreManager.Instance.Score += 100;    //빗방울 ground와 충돌 할 때 마다 점수 표시
+
+            Destroy(this.gameObject);  //빗방울 제거
+
+        }
+
+        if (other.gameObject.tag == "Ground")       //빗방울 ground와 충돌 시
+        {
+
+            Destroy(this.gameObject);  //빗방울 제거
+
+        }
+
+    }
+
+}
