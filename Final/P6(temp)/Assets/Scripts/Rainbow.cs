7dd25b92feb0a0e8fe587e74d54e8dc0564f689f
eunsooklee <98075420+eunsooklee@users.noreply.github.com>
2022-02-03 15:12:04 +0900

    2.3.15
    온도계부분코드만 고침

---
+++ Final/P6(temp)/Assets/Scripts/Rainbow.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rainbow : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = Vector2.down;
+        transform.position += dir * Time.deltaTime*10;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")   //레인보우 플레이어와 충돌 시
+        {
+            ScoreManager.Instance.Score += 200;
+            Destroy(gameObject);  //레인보우 제거
+
+
+        }
+
+        if (other.gameObject.tag == "Ground")       //레인보우 ground와 충돌 시
+        {
+
+
+            Destroy(gameObject);  //레인보우 제거
+
+        }
+
+    }
+}
---
+++ Final/P6(temp)/Assets/Scripts/SceneLoadBtn.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadBtn : MonoBehaviour
+{
+    public void StartClick(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
